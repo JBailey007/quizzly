aa6c0bf7c5c9450bccc6ff09ad5cb4e401060b08
Jalen Bailey <jwbailey13@gmail.com>
2023-07-09 14:12:12 -0600

    Created a container that holds the questions and answers, It also allows me to start the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,71 @@
-var questionOne = {
-    question: "Commonly used data types DO NOT include:",
-    answer: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    correctAnswer: 2
+const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
+const questionContainerElement = document.getElementById("question-container")
+const questionElement = document.getElementById("questions")
+const answerButtonsElement = document.getElementById("answer-buttons")
+const correctText = document.getElementById("text-correct")
+
+let shuffledQuestions, currentQuestionsIndex
+
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click" , () => {
+    currentQuestionsIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+startButton.classList.add("hide");
+shuffledQuestions = question.sort(() => Math.random() - .5)
+currentQuestionsIndex = 0
+questionContainerElement.classList.remove("hide")
+setNextQuestion();
 }
 
-function showQuestion() {
-    console.log("Showing Questions")
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionsIndex])
 }
 
-var button = document.querySelector("button");
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild (answerButtonsElement.firstChild)
+    }
+}
 
-button.addEventListener("click", function(event) {
-    event.preventDefault();
-    showQuestion();
-    console.log("click");
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach( answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+        })
+}
 
-});
+function selectAnswer(i) {
+    const selectedButton = i.target
+    const correct = selectedButton.dataset.correct
+    if (correct) {
+        correctText.classList.remove("hide")
+        correctText.innerText = "correct"        
+    }
+    // Else remove time from the timer that you are going to create
 
+}
 
-
-
-
-
-
-
-
-
-
-// var questionTwo = {
-//     question: "Commonly used data types DO NOT include:",
-//     answer: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-//     correctAnswer: 2
-// }
-
-// var questionsThree = {
-//     question: "Commonly used data types DO NOT include:",
-//     answer: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-//     correctAnswer: 2
-// }
-
-// var questionFour = {
-//     question: "Commonly used data types DO NOT include:",
-//     answer: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-//     correctAnswer: 2
-// }
+const question = [
+    {
+        question: "Javascript is an _______ language?",
+        answers: [
+            {text: "Object Oriented", correct: true }, 
+            {text: "Object Based", correct: false},
+            {text: "Procedural", correct: false}, 
+            {text: "None of the Above", correct: false}  
+        ]
+    }
+]
